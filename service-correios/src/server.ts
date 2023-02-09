cffc3a59eb7b6be946db8a5293f6679c4278f7c9
João Alves <joao.alves@ecosis.com.br>
2023-02-09 17:16:17 -0300

    :construction: Create microservice service-correios

---
+++ service-correios/src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import correios from 'correios.js'
+import z from 'zod'
+
+const app = express()
+app.use(express.json())
+
+app.get('/track', async (req, res) => {
+    try {
+        const trackQueryParams = z.object({
+            code: z.string().url(),
+        })
+
+        const {code} = trackQueryParams.parse(req.query)
+        const response = await correios.track(code)
+
+        res.send(response).json().status(200)
+
+    } catch (error) {
+        res.status(500)
+        res.send({error: `Não foi possível completar a requisação: ${error}`}).json()
+    }
+})
+
+app.listen(3002, () => {
+    console.log('Microsserviço iniciado na porta 3002')
+})
